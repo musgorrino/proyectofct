785b08be745e812cf887dbe876a6985817f0c6f7
Ruiz9612 <36847338+Ruiz9612@users.noreply.github.com>
2018-02-27 09:40:34 +0100

    hola

--- ruiz/index.php
+++ ruiz/index.php
@@ -10,6 +10,13 @@ cabecera("Gestion FCT", "estilos.css");
 		<div id="logo">
 		</div>
 		<div id="menu">
+			
+				<ul id="menu">
+				  <li><a href="#home">Home</a></li>
+				  <li><a href="#news">News</a></li>
+				  <li><a href="#contact">Contact</a></li>
+				  <li><a href="#about">About</a></li>
+				</ul>
 		</div>
 		<div id="submenu">
 		</div>	
