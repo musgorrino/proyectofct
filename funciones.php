c8b5223a679fc4eb3c8eff076d3c2576e38ee9a3
musgorrino <musgorrino@users.noreply.github.com>
2018-02-27 11:22:14 +0100

    yyh

--- funciones.php
+++ funciones.php
@@ -188,5 +188,45 @@ var_dump($_POST["dni"]);
         }
         var_dump($datos);
     }
+    function crear_tablas($array)
+    {
+        $conexion=mysqli_connect("localhost","root","","mydb");
+        $recogida="select * from mydb.alumnos";
+        $datos=mysqli_query($conexion,$recogida);
+        $totalfilas= mysqli_num_rows($datos);
+
+?>      <h3>
+        alumnos
+    </h3>
+        <table border=1>
+            <tr><td>codigo</td><td>Nombre</td><td>Apellidos</td><td>dni</td><td>fechanac</td><td>telefono</td><td>email</td><td>euskera</td><td>carnet</td><td>otros</td></tr>
+        <?php
+        if ($totalfilas>0){
+        $_fila=mysqli_fetch_assoc($datos);
+        while ($_fila)
+        {
+        ?>
+            <tr>
+                <td><?php echo $_fila["codigo"];?></td>
+                <td><?php echo $_fila["nombre"];?></td>
+                <td><?php echo $_fila["apellidos"];?></td>
+                <td><?php echo $_fila["dni"];?></td>
+                <td><?php echo $_fila["fechanac"];?></td>
+                <td><?php echo $_fila["telefono"];?></td>
+                <td><?php echo $_fila["email"];?></td>
+                <td><?php echo $_fila["euskera"];?></td>
+                <td><?php echo $_fila["carnet"];?></td>
+                <td><?php echo $_fila["otros"];?></td>
+            <tr>
+                <?php
+                $_fila=mysqli_fetch_assoc($datos);
+                }
+                }
+                ?>
+        </table>
+        <?php
+        mysqli_close($conexion);
+
+    }
 
 }
