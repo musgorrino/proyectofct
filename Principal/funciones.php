dc1a4efdd4af200d782e40f4c5ecf48b02c91195
Ruiz9612 <36847338+Ruiz9612@users.noreply.github.com>
2018-02-27 11:47:28 +0100

    hola

--- Principal/funciones.php
+++ Principal/funciones.php
@@ -6,7 +6,7 @@ function cabecera($titulo, $css)
    <html xmlns="http://www.w3.org/1999/xhtml">
   <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-  <title><?php echo $texto ?></title>
+  <title><?php echo $titulo ?></title>
   <link href="<?php echo $css ?>" rel="stylesheet" type="text/css" />
 </head>
 <body>
