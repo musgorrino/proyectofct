c941d0bf2bc0b2ad4e8efdb9430b51b39bd706fa
musgorrino <musgorrino@users.noreply.github.com>
2018-02-27 10:04:17 +0100

    h

--- pruebas.php
+++ pruebas.php
@@ -5,3 +5,13 @@
  * Date: 27/02/2018
  * Time: 10:02
  */
+$dni = recogedni($_POST["dni"]);
+$nombre = recogvarc($_POST["nombre"]);
+$apellidos = recogvarc($_POST["apellidos"]);
+$direcion = recoger($_POST["direccion"]);
+$sexo = recoger($_POST["sexo"]);
+$fijo = recogernum($_POST["telefono_fijo"]);
+$movil = recogernum($_POST["telefono_movil"]);
+$usuario = recoger($_POST["usuario"]);
+$contrasena = recoger($_POST["pass"]);
+$recontra = recoger($_POST["reppass"]);*/
