bcf43e8d73f451c41e72bfdc7cac9d7359f67447
musgorrino <musgorrino@users.noreply.github.com>
2018-02-27 09:21:23 +0100

    Crecions de funciones de control y primera insercion

--- funciones.php
+++ funciones.php
@@ -16,22 +16,8 @@ function recogedni($dni)
     else
     {
 
-        if(comprobarDniu($dni)==2)
-        {
-            return 2;
-        }
-        else
-        {
-            if (comprobarDnif($dni)==3)
-            {
-                return 3;
 
-            }
-            else
-            {
                 return 0;
-            }
-        }
     }
 
 }
@@ -82,4 +68,64 @@ function comprobarnum($valor)
     {
         return false;
     }
+}
+function comprobarnonum($valor)
+{
+
+    if(preg_match("|^[a-zA-Z]+(\s*[a-zA-Z]*)*[a-zA-Z]+$|",$valor)){
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+function conectar ($host,$user,$pass,$bd)
+{
+    $conexion = mysqli_connect ($host, $user, $pass,$bd);
+    return $conexion;
+}
+function poner_datos($dato)
+{
+    if(isset($_POST[$dato]))
+    {
+        return $_POST[$dato];
+    }
+}
+function login($usuario,$contrasena)
+{
+    $conexion=conectar("localhost","root","","asociaciones");
+    $query="select usuario, contrasena from usuario where usuario='".$usuario."' and contrasena='".$contrasena."'";
+    $r=mysqli_query ($conexion,$query);
+    $totalfilas=mysqli_num_rows($r);
+    if($totalfilas==1)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+function alta_alumno()
+{
+
+
+     $dni = recogedni($_POST["dni"]);
+
+     $nombre = recogvarc($_POST["nombre"]);
+     $apellidos = recogvarc($_POST["apellidos"]);
+     $fechanac = recoger($_POST["fecha"]);
+     $telefono = recogernum($_POST["telefono"]);
+     $email = recoger($_POST["email"]);
+     $euskera = recoger($_POST["euskera"]);
+     $carnet = recoger($_POST["carnet"]);
+     $otros = recoger($_POST["otros"]);
+
+    $conexion=conectar("localhost","root","","bd");
+    $query="insert into usuario (nombre,contrasena,dni,usuario,apellidos,direccion,sexo,telefijo,telemovil) values('".$_POST["nombre"]."','".$_POST["pass"]."','".$_POST["dni"]."','".$_POST["usuario"]."','".$_POST["apellidos"]."','".$_POST["direccion"]."','".$_POST["sexo"]."',".$_POST["telefono_fijo"].",".$_POST["telefono_movil"].")";
+
+    $r=mysqli_query ($conexion,$query) /*or die(mysqli_error())*/;
+
+
 }
