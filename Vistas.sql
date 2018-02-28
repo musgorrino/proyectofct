75e63a5f084c7dbe8e28397fab016f081a5698c8
musgorrino <musgorrino@users.noreply.github.com>
2018-02-28 13:10:14 +0100

    modificacion vistas

--- Vistas.sql
+++ Vistas.sql
@@ -3,6 +3,9 @@ CREATE VIEW `grupo`
 from grupos g,ciclos_formativos c,profesores p,profesores r
 where g.ciclo=c.codigo and g.tutor=p.codigo and g.tutor_practicas=r.codigo;
 
+
+
+Segunda vista incompleta
 SELECT p.codigo, codigointerprof, p.nombre,apellidos,telefono,email,
 ifnull(g.abreviatura,"No es tutor de un grupo") AS "Tutor de grupo",
 ifnull(f.abreviatura,"No es tutor de practicas") as "Tutor de practicas",
@@ -11,3 +14,13 @@ FROM profesores p left outer join
 ((select codigo, abreviatura,tutor from grupos) g,(select codigo, abreviatura,tutor_practicas from grupos) f
 ,familias_profesionales t) 
 ON (p.codigo=g.tutor or p.codigo=f.tutor_practicas) and p.codigo=t.coordinador
+
+/*v2*/
+SELECT p.codigo, codigointerprof, p.nombre,apellidos,telefono,email,ifnull(g.abreviatura,"No es tutor de un grupo") 
+AS "Tutor de grupo",ifnull(f.abreviatura,"No es tutor de practicas") as "Tutor de practicas" 
+FROM profesores p left outer join ((select codigo, abreviatura,tutor from grupos) g,
+(select codigo, abreviatura,tutor_practicas from grupos) f) ON p.codigo=g.codigo and p.codigo=f.codigo
+
+SELECT * FROM profesores p left outer join ((select abreviatura,tutor from grupos)
+ g,(select abreviatura,tutor_practicas from grupos) f) 
+ON p.codigo=f.tutor_practicas or p.codigo=g.tutor
