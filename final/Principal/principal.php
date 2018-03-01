772239ab3d8bfd67ce3d375f82b79521aa08879d
Muhamadu <muhamaduladur@gmail.com>
2018-03-01 10:24:19 +0100

    Y LA EUROPEA?

--- final/Principal/principal.php
+++ final/Principal/principal.php
@@ -8,21 +8,22 @@ cabecera("Gestion FCT", "estilos.css");
 		<div id="login">
 		</div>
 		<div id="logo">
-			<img src="../imagenes/logo.png">
+			<img src="../imagenes/logo.png"> 
 		</div>
-		
+		</br>
+		</br>
 		<div id="menu">
 			
 				<ul>
 				<li class="espacio">----------------</li>
-				  <li><a href="#home">Familias Profesionales</a></li>
-				  <li><a href="#news">Ciclos Formativos</a></li>
-				  <li><a href="#contact">Grupos</a></li>
-				  <li><a href="#about">Profesores</a></li>
-				  <li><a href="#home">Alumnos </a></li>
-				  <li><a href="#news">Asignaciones</a></li>
-				  <li><a href="#contact">Empresas</a></li>
-				  <li><a href="#about">Responsables</a></li>
+				  <li><a href="#Familias">Familias Profesionales</a></li>
+				  <li><a href="#Ciclos">Ciclos Formativos</a></li>
+				  <li><a href="#Grupos">Grupos</a></li>
+				  <li><a href="#Profesores">Profesores</a></li>
+				  <li><a href="#Alumnos">Alumnos </a></li>
+				  <li><a href="#Asignaciones">Asignaciones</a></li>
+				  <li><a href="#Empresas">Empresas</a></li>
+				  <li><a href="#Responsables">Responsables</a></li>
 				</ul>				
 		</div>
 		
