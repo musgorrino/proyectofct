4d9154f8f09bc24baa145c47bb2029e4cbcbabb7
musgorrino <musgorrino@users.noreply.github.com>
2018-03-02 12:07:34 +0100

    prueba insert dinamica

--- pruebas.php
+++ pruebas.php
@@ -1,4 +1,18 @@
 <?php
 include "funciones.php";
 
-/*coger_tablas("profesor");?>
+/*coger_tablas("profesor");*/
+if (isset($_POST["enviar"]))
+{
+    ob_get_length($_POST);
+    var_dump($_POST);
+}
+
+?>
+<form method="post" action="pruebas.php">
+    <input type="hidden" name="cosa1" value="1">
+    <input type="hidden" name="cosa2" value="2">
+    <input type="hidden" name="cosa3" value="3">
+    <input type="hidden" name="cosa4" value="4">
+    <input type="submit" name="enviar" value="enviar">
+</form>
