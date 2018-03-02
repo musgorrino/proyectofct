2e986a36c2d85ad8e5b205442ebfd13a072092b6
Muhamadu <muhamaduladur@gmail.com>
2018-03-02 08:55:20 +0100

    efcec

--- final/Principal/principal.php
+++ final/Principal/principal.php
@@ -15,25 +15,79 @@ cabecera("Gestion FCT", "estilos.css");
 			
 				<ul>
 				<li class="espacio">----------------</li>
-				  <li><a href="../familias.php">Familias Profesionales</a></li>
-				  <li><a href="../ciclos.php">Ciclos Formativos</a></li>
-				  <li><a href="../grupos.php">Grupos</a></li>
-				  <li><a href="../profesores.php">Profesores</a></li>
-				  <li><a href="../alumnos.php">Alumnos </a></li>
-				  <li><a href="../asignaciones.php">Asignaciones</a></li>
-				  <li><a href="../empresas.php">Empresas</a></li>
-				  <li><a href="../responsables.php">Responsables</a></li>
+				  <li><a href="#Familias">Familias Profesionales</a></li>
+				  <li><a href="#Ciclos">Ciclos Formativos</a></li>
+				  <li><a href="#Grupos">Grupos</a></li>
+				  <li><a href="#Profesores">Profesores</a></li>
+				  <li><a href="#Alumnos">Alumnos </a></li>
+				  <li><a href="#Asignaciones">Asignaciones</a></li>
+				  <li><a href="#Empresas">Empresas</a></li>
+				  <li><a href="#Responsables">Responsables</a></li>
 				</ul>				
 		</div>
 		<center><h3> GESTION DE FORMACIÓN EN CENTRO DE TRABAJO </h3> </center>
-		<table border="4">
 		
-			
-		</table>
 	</div>
 	
 	
 <div id="contenido">
+		<table id="customers">
+  <tr>
+    <th>Company</th>
+    <th>Contact</th>
+    <th>Country</th>
+  </tr>
+  <tr>
+    <td>Alfreds Futterkiste</td>
+    <td>Maria Anders</td>
+    <td>Germany</td>
+  </tr>
+  <tr>
+    <td>Berglunds snabbköp</td>
+    <td>Christina Berglund</td>
+    <td>Sweden</td>
+  </tr>
+  <tr>
+    <td>Centro comercial Moctezuma</td>
+    <td>Francisco Chang</td>
+    <td>Mexico</td>
+  </tr>
+  <tr>
+    <td>Ernst Handel</td>
+    <td>Roland Mendel</td>
+    <td>Austria</td>
+  </tr>
+  <tr>
+    <td>Island Trading</td>
+    <td>Helen Bennett</td>
+    <td>UK</td>
+  </tr>
+  <tr>
+    <td>Königlich Essen</td>
+    <td>Philip Cramer</td>
+    <td>Germany</td>
+  </tr>
+  <tr>
+    <td>Laughing Bacchus Winecellars</td>
+    <td>Yoshi Tannamuri</td>
+    <td>Canada</td>
+  </tr>
+  <tr>
+    <td>Magazzini Alimentari Riuniti</td>
+    <td>Giovanni Rovelli</td>
+    <td>Italy</td>
+  </tr>
+  <tr>
+    <td>North/South</td>
+    <td>Simon Crowther</td>
+    <td>UK</td>
+  </tr>
+  <tr>
+    <td>Paris spécialités</td>
+    <td>Marie Bertrand</td>
+    <td>France</td>
+  </tr>
+</table>
 		</div>
 		
 <div id="pie">
