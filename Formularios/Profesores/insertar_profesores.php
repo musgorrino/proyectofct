ff46d2847fa0af0c7beb2ebef6cf3454f9862bf1
Ruiz9612 <36847338+Ruiz9612@users.noreply.github.com>
2018-03-05 13:17:23 +0100

    hola

--- Formularios/Profesores/insertar_profesores.php
+++ Formularios/Profesores/insertar_profesores.php
@@ -9,32 +9,28 @@ cabecera("Insertar Profesor.","estilos.css");
 				<tr>
 					<th><label  for="codigointerprof">Codigo Interno:</label></th> 
 					<td><input type="text" id="codigointerprof" name="codigointerprof" size="20" value="" /></td>
-				</tr>
-				<tr>
+				
 					<th><label  for="nombre">Nombre:</label></th>
 					<td><input id="nombre" type="text"  name="nombre" size="20" value="" /></td>
 				</tr>
 				<tr>
 					<th><label  for="apellidos">Apellidos:</label></th>
 					<td><input id="apellidos" type="text"  name="apellidos" size="40" value="" /></td>
-				</tr>
-				<tr>
+				
 					<th><label  for="telefono">Telefono:</label></th>
 					<td><input id="telefono"type="text"  name="telefono" size="20" value="" /></td>
 				</tr>
 				<tr>
 					<th><label  for="email">Email:</label></th>
 					<td><input id="email" type="text"  name="email" size="20" value="" /></td>
-				</tr>
+				
+					<td><input type="reset" id="limpiar" name="limpiar" value="Limpiar" /></td>
+					<td><input type="submit" id="enviar" name="enviar" value="Guardar" /></td>
 				
 			</table>
-	<p>
+	
 		<input type="hidden" name="codigo" value="<?php echo $codigo; ?>" />
-	</p>
-	<p>
-        <input type="reset" id="limpiar" name="limpiar" value="Limpiar" />
-		<input type="submit" id="enviar" name="enviar" value="Guardar" />
-	</p>
+	
 </form>
 
 
