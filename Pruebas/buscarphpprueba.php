3d9ad7bbcf1fb7e5fb7d4a28ee6c35145bb9f3f8
samusfusion <36847075+samusfusion@users.noreply.github.com>
2018-03-06 08:27:09 +0100

    10

---
+++ Pruebas/buscarphpprueba.php
@@ -0,0 +1,26 @@
+<?php 
+include("../../funciones/funciones.php");
+
+carnet euskera nombre
+$nombre=recoger($_Post['nombre']);
+$uskera=recoger($_Post['euskera']);
+$carnet=recoger($_Post['carnet']);
+$ciclo=recoger($_Post['ciclo']);
+$curso=recoger($_Post['curso']);
+if($nombre==1){
+echo
+}
+if($uskera==1){
+echo
+}
+if($carnet==1){
+echo
+}
+if($ciclo==1){
+echo
+}
+if($curso==1){
+echo
+}
+
+?>
