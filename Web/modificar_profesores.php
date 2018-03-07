36923011fcebed7e3fa0d8158dcfc363e04f0aa1
Ruiz9612 <36847338+Ruiz9612@users.noreply.github.com>
2018-03-07 13:03:32 +0100

    hola

--- Web/modificar_profesores.php
+++ Web/modificar_profesores.php
@@ -17,25 +17,9 @@ cabecera("Gestion FCT", "estilo.css");
 	</div>
 	
 	<div id="contenido">
-	<?
-	 conexion_bbdd(){
-		$conexion=conectar("mydb"); 
- 
-		$codigo=$_POST['codigo'];
- 
-		$q = "select * from profesores where codigo ='$codigo'";
-		$resultado = mysql_query($q,$conexion) or die(mysql_error());
-		$total= mysql_num_rows($resultado);
- 
-	if ($total>0){
-		$fila = mysql_fetch_assoc($resultado);		
-	}
-		 modificar_profesores();
-		 
-	mysql_free_result($resultado);
-	mysql_close($conexion);
-	}
-		 ?>
+	
+	
+		 <?php modificar_profesores();?>
 	</div>	
 	<div id="pie">
 		<?php ver_pie(); ?>
--- funciones_insertar.php
+++ funciones_insertar.php
@@ -563,21 +563,21 @@ if(isset($_POST['enviar'])){
 				<table>
 					<tr>
 						<td><label for="codigointerprof">Codigo Interno:</label></th> 
-						<td><input type="text" id="codigointerprof" name="codigointerprof" size="20" value="<?php echo $fila['codigointerprof']; ?>" /></td>
+						<td><input type="text" id="codigointerprof" name="codigointerprof" size="20" value="" /></td>
 					
 						<td><label for="nombre">Nombre:</label></td>
-						<td><input id="nombre"  type="text"  name="nombre" size="20" value="<?php echo $fila['nombre']; ?>" /></td>
+						<td><input id="nombre"  type="text"  name="nombre" size="20" value="" /></td>
 					</tr>
 					<tr>
 						<td><label for="apellidos">Apellidos:</label></td>
-						<td><input id="apellidos" type="text"  name="apellidos" size="40" value="<?php echo $fila['apellidos']; ?>" /></td>
+						<td><input id="apellidos" type="text"  name="apellidos" size="40" value="" /></td>
 					
 						<td><label for="telefono">Telefono:</label></td>
-						<td><input id="telefono type="text"  name="telefono" size="20" value="<?php echo $fila['telefono']; ?>" /></td>
+						<td><input id="telefono type="text"  name="telefono" size="20" value="" /></td>
 					</tr>
 					<tr>
 						<td><label for="email">Email:</label></td>
-						<td><input id="email" type="text"  name="email" size="20" value="<?php echo $fila['email']; ?>" /></td>
+						<td><input id="email" type="text"  name="email" size="20" value="" /></td>
 					
 						<td><input class="botones" type="reset" id="limpiar" name="limpiar" value="Limpiar" /></td>
 						<td><input class="botones" type="submit" id="enviar" name="enviar" value="Modificar" /></td>
@@ -675,17 +675,17 @@ if(isset($_POST['enviar'])){
 		</tr>
 		<tr>
 			<td><label for="curso">Curso: </label></td>
-			<td><input type="text" name="curso" value="<?php echo $fila['curso']; ?>" id="curso"></td>
+			<td><input type="text" name="curso" value="" id="curso"></td>
 		
 			<td><label for="horario">Horario: </label></td>
-			<td><input type="text" name="horario" value="<?php echo $fila['horario']; ?>" id="horario"></td>
+			<td><input type="text" name="horario" value="" id="horario"></td>
 		</tr>
 		<tr>
 			<td><label for="observaciones">Observaciones: </label></td>
-			<td><textarea name="observaciones" id="observaciones"><?php echo $fila['observaciones']; ?></textarea></td>
+			<td><textarea name="observaciones" id="observaciones"></textarea></td>
 		
 			<td><label for="trabajodesempenado">Trabajo desempeñado: </label></td>
-			<td><textarea name="trabajodesempenado" id="trabajodesempenado"><?php echo $fila['trabajodesempenado']; ?></textarea></td>
+			<td><textarea name="trabajodesempenado" id="trabajodesempenado"></textarea></td>
 		</tr>
 		<tr>
 			<td>Contratacion del alumno:</td>
@@ -718,56 +718,56 @@ if(isset($_POST['enviar'])){
 			<table>
 				<tr>
 					<td><label for="nombre">Nombre: </label></td>
-					<td><input type="text" name="nombre" value="<?php echo $Post['nombre']; ?>" id="nombre"/></td>
+					<td><input type="text" name="nombre" value="" id="nombre"/></td>
 				
 					<td><label for="nif">Nif: </label></td>
-					<td><input type="text" name="nif" value="<?php echo $Post['nif']; ?>" id="nif"/></td>
+					<td><input type="text" name="nif" value="" id="nif"/></td>
 				</tr>
 				<tr>
 					<td><label for="titularidad">Titularidad: </label></td>
-					<td><input type="text" name="titularidad" value="<?php echo $Post['titularidad']; ?>" id="titularidad"/></td>
+					<td><input type="text" name="titularidad" value="" id="titularidad"/></td>
 				
 					<td><label for="direccion">Direccion: </label></td>
-					<td><input type="text" name="direccion" value="<?php echo $Post['direccion']; ?>" id="direccion"/></td>
+					<td><input type="text" name="direccion" value="" id="direccion"/></td>
 				</tr>
 				<tr>
 					<td><label for="poblacion">Poblacion: </label></td>
-					<td><input type="text" name="poblacion" value="<?php echo $Post['poblacion']; ?>" id="poblacion"/></td>
+					<td><input type="text" name="poblacion" value="" id="poblacion"/></td>
 				
 					<td><label for="provincia">Provincia: </label></td>
-					<td><input type="text" name="provincia" value="<?php echo $Post['provincia']; ?>" id="provincia"/></td>
+					<td><input type="text" name="provincia" value="" id="provincia"/></td>
 				</tr>
 				<tr>
 					<td><label for="cp">Codigo Postal: </label></td>
-					<td><input type="text" name="cp" value="<?php echo $Post['cp']; ?>" id="cp"></td>
+					<td><input type="text" name="cp" value="" id="cp"></td>
 				
 					<td><label for="telefono">Telefono: </label></td>
-					<td><input type="text" name="telefono" value="<?php echo $Post['telefono']; ?>" id="telefono"/></td>
+					<td><input type="text" name="telefono" value=">" id="telefono"/></td>
 				</tr>
 				<tr>
 					<td><label for="fax">Fax: </label></td>
-					<td><input type="text" name="fax" value="<?php echo $Post['fax']; ?>" id="fax"/></td>
+					<td><input type="text" name="fax" value="" id="fax"/></td>
 			
 					<td><label for="repempresa">Representante de la empresa: </label></td>
-					<td><input type="text" name="repempresa" value="<?php echo $Post['repempresa']; ?>" id="repempresa"/></td>
+					<td><input type="text" name="repempresa" value="" id="repempresa"/></td>
 				</tr>
 				<tr>
 					<td><label for="personacontacto">Persona de contacto: </label></td>			
-					<td><input type="text" name="personacontacto" value="<?php echo $Post['personacontacto']; ?>" id="personacontacto"/></td>
+					<td><input type="text" name="personacontacto" value="" id="personacontacto"/></td>
 				
 					<td><label for="actividadempresa">Actividad de la empresa: </label></td>
-					<td><input type="text" name="actividadempresa" value="<?php echo $Post['actividadempresa']; ?>" id="actividadempresa"/></td>
+					<td><input type="text" name="actividadempresa" value="" id="actividadempresa"/></td>
 				</tr>
 				<tr>
 					<td><label for="numtrabajadores">Numero de trabajadores: </label></td>
-					<td><input type="text" name="numtrabajadores" value="<?php echo $Post['numtrabajadores']; ?>" id="numtrabajadores"/></td>
+					<td><input type="text" name="numtrabajadores" value="" id="numtrabajadores"/></td>
 				
 					<td><label for="kmscentro">Kilometros desde el centro: </label></td>
-					<td><input type="text" name="kmscentro" value="<?php echo $Post['kmscentro']; ?>" id="kmscentro"/></td>
+					<td><input type="text" name="kmscentro" value="" id="kmscentro"/></td>
 				</tr>
 				<tr>
 					<td><label for="horariopracticas">Horario de las practicas: </label></td>	
-					<td><input type="text" name="horariopracticas" value="<?php echo $Post['horariopracticas']; ?>" id="horariopracticas"/></td>
+					<td><input type="text" name="horariopracticas" value="" id="horariopracticas"/></td>
 				
 					<td>Convenio:</td>
 					<td>
@@ -798,7 +798,7 @@ if(isset($_POST['enviar'])){
 	<table>
 		<tr>
 			<td><label for="nombre">Nombre: </label></td>
-			<td><input type="text" name="nombre" value="<?php echo $fila['nombre']; ?>" id="nombre"></td>
+			<td><input type="text" name="nombre" value="" id="nombre"></td>
 		
 			<td><label for="empresa">Empresa: </label></td>
 			<td><select id="empresa">
