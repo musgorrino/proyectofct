82d906fa0f98679740b48b36f34d6fd8f89c70f0
samusfusion <36847075+samusfusion@users.noreply.github.com>
2018-03-07 11:01:39 +0100

    2

--- buscar/buscarhistori.php
+++ buscar/buscarhistori.php
@@ -2,6 +2,7 @@
 include("../funciones.php");
  
 cabecera("Gestion FCT", "../Web/estilo.css");
+if(isset($_POST['enviar'])){
 ?>
 <div id="contenedor">
 	<div id="cabecera">
@@ -45,6 +46,54 @@ cabecera("Gestion FCT", "../Web/estilo.css");
 		<?php ver_pie(); ?>
 	</div>
 </div>
-
+<?php
+}
+else {
+?>
+<div id="contenedor">
+	<div id="cabecera">
+		<div id="login">
+		</div>
+		<div id="logo">
+			<?php ver_logo("../imagenes/logo.png");?>
+		</div>
+		<div id="menu">
+			<?php ver_menu();?>				
+		</div>
+	</div>
+	
+	<div id="contenido">
+		<?php ver_buscar("historiales","Historiales","los historiales","un historial nuevo");?>
+		<div id="formbuscar">
+			<h3> Buscar informacion de los Historiales </h3>
+			<table>
+				<form action="buscarhistori.php" method="post">
+					<tr>
+						<td><label for="grupo">Grupo </label></td>
+						<td><input type="text" id="grupo" name="grupo" value="" placeholder="Inserte nombre grupo"></td>	
+					</tr>	
+					<tr>	
+						<td><label for="alumno">Alumno</label></td>
+						<td><input type="text" id="alumno"	name="alumno" value="" placeholder="Inserte nombre alumno"/></td>
+					</tr>
+					<tr>
+						<td><label for="curso">Curso</label></td>
+						<td><input type="text" id="curso"	name="curso" value="" placeholder="Inserte curso"/></td>
+					</tr>
+						<td><input type="hidden" name="" value=""></td>
+						<td><input type="submit" name="enviar" value="BUSCAR"/>
+					</tr>
+				</form>
+			</table>
+		</div>
+	</div>
+		
+	<div id="pie">
+		<?php ver_pie(); ?>
+	</div>
+</div>
+<?php
+}
+?>
 </body>
 </html>
